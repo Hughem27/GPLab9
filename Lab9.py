c4fef856d71aa81a1ca7c2687e7d895b8453006d
Hughem27 <markhughesster@gmail.com>
2023-12-15 10:14:25 +0000

    L9 RGB Complete

--- Lab9.py
+++ Lab9.py
@@ -1,40 +1,81 @@
 import cv2
 import numpy as np
-import copy
 from matplotlib import pyplot as plt
 
-
-
-# Load orgImg
-imgOrig = cv2.imread(r'C:\Lab9\ATU1.jpg')
-img1 = cv2.imread(r'C:\Lab9\OperaHouse.jpg')  
+# Load the images
+img1 = cv2.imread(r'C:\Lab9\OperaHouse.jpg')  # Update with the correct path if needed
 img2 = cv2.imread(r'C:\Lab9\ATU2.jpg')
+# Load the image
+img = cv2.imread(r'C:\Lab9\OperaHouse.jpg')  # Update with the correct path to your image
 
-# Convert org img to grayscale
-imgGray = cv2.cvtColor(imgOrig, cv2.COLOR_BGR2GRAY)
-# Perform Harris corner detection
-blockSize = 2
-aperture_size = 3
-k = 0.04
-dst = cv2.cornerHarris(imgGray, blockSize, aperture_size, k)
-# Create a deep copy of the original image
-imgHarris = copy.deepcopy(imgOrig)
-# Set the threshold for corner detection
-threshold = 0.01
-# Loop through every element in the dst matrix
-for i in range(len(dst)):
-    for j in range(len(dst[i])):
-        # Check if the element is greater than the threshold
-        if dst[i][j] > (threshold * dst.max()):
-            # Draw a circle on the imgHarris image
-            cv2.circle(imgHarris, (j, i), 3, (0, 0, 255), -1)  # Red color for circles
+# Convert the images to grayscale
+gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+# Split the image into channels
+b, g, r = cv2.split(img)
 
-# Plot the image with Harris corners
-plt.imshow(cv2.cvtColor(imgHarris, cv2.COLOR_BGR2RGB))
-plt.title('Harris Corners')
-plt.show()
+# Initiate ORB detector
+orb = cv2.ORB_create()
+# Create a Matplotlib subplot
+plt.figure(figsize=(10, 4))
 
-# Plot the result
-plt.imshow(cv2.cvtColor(imgHarris, cv2.COLOR_BGR2RGB))
-plt.title('Harris Corner Detection with Circles')
+# Find the keypoints and descriptors with ORB
+kp1, des1 = orb.detectAndCompute(gray1, None)
+kp2, des2 = orb.detectAndCompute(gray2, None)
+# Display the original image
+plt.subplot(1, 4, 1), plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB)), plt.title('Original')
+plt.axis('off')
+
+# Brute-Force Matcher
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+matches_bf = bf.match(des1, des2)
+# Display the Red channel
+plt.subplot(1, 4, 2), plt.imshow(r, cmap='gray'), plt.title('Red Channel')
+plt.axis('off')
+
+# Sort them in ascending order of distance
+matches_bf = sorted(matches_bf, key=lambda x: x.distance)
+# Display the Green channel
+plt.subplot(1, 4, 3), plt.imshow(g, cmap='gray'), plt.title('Green Channel')
+plt.axis('off')
+
+# Draw matches
+img_matches_bf = cv2.drawMatches(img1, kp1, img2, kp2, matches_bf[:10], None, flags=2)
+# Display the Blue channel
+plt.subplot(1, 4, 4), plt.imshow(b, cmap='gray'), plt.title('Blue Channel')
+plt.axis('off')
+
+# Display the result for the first pair of images
+plt.figure(figsize=(8, 4))
+plt.subplot(121), plt.imshow(img_matches_bf), plt.title('BruteForceMatcher - House vs ATU2')
+
+# Load new images
+img1_new = cv2.imread(r'C:\Lab9\castle.jpg')
+img2_new = cv2.imread(r'C:\Lab9\Sky.jpg')
+
+# Convert the new images to grayscale
+gray1_new = cv2.cvtColor(img1_new, cv2.COLOR_BGR2GRAY)
+gray2_new = cv2.cvtColor(img2_new, cv2.COLOR_BGR2GRAY)
+
+# Find the keypoints and descriptors with ORB for the new images
+kp1_new, des1_new = orb.detectAndCompute(gray1_new, None)
+kp2_new, des2_new = orb.detectAndCompute(gray2_new, None)
+
+# Brute-Force Matcher for the new images
+matches_bf_new = bf.match(des1_new, des2_new)
+
+# Sort them in ascending order of distance
+matches_bf_new = sorted(matches_bf_new, key=lambda x: x.distance)
+
+# Draw matches for the new images
+img_matches_bf_new = cv2.drawMatches(img1_new, kp1_new, img2_new, kp2_new, matches_bf_new[:10], None, flags=2)
+
+# Display the result for the second pair of images
+plt.subplot(122), plt.imshow(img_matches_bf_new), plt.title('BruteForceMatcher - Dail vs Kitchen')
+
+# Adjust layout for better display
+plt.tight_layout()
+
+# Show the plots
+# Show the plot
 plt.show()
