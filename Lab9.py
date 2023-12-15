f7fd396beba48464ffbdacf3d4d9f76093f61aa8
Hughem27 <markhughesster@gmail.com>
2023-12-15 09:44:50 +0000

    L9 Brute Foce Matcher & Flann

--- Lab9.py
+++ Lab9.py
@@ -1,57 +1,84 @@
+import numpy as np
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-dst = cv2.cornerHarris(imgGray, blockSize, aperture_size, k)
+# Load the image
+imgOrig = cv2.imread(r'C:\Lab9\ATU2.jpg')
 
-# Create a deep copy of the original image
-# Create a deep copy of the original image for drawing Harris circles
-imgHarris = copy.deepcopy(imgOrig)
+# Check if the image is loaded successfully
+if imgOrig is None:
+    print("Error: Could not open or read the image.")
+else:
+    print("Image loaded successfully.")
 
-# Set the threshold for corner detection
-threshold = 0.01
-# Set the threshold for Harris corner detection
-threshold_harris = 0.01
+# Convert the original image to grayscale
+imgGray = cv2.cvtColor(imgOrig, cv2.COLOR_BGR2GRAY)
 
-# Loop through every element in the dst matrix
-for i in range(len(dst)):
-    for j in range(len(dst[i])):
-        # Check if the element is greater than the threshold
-        if dst[i][j] > (threshold * dst.max()):
-            # Draw a circle on the imgHarris image
-            cv2.circle(imgHarris, (j, i), 3, (0, 0, 255), -1)  # Red color for circles
-        if dst[i][j] > (threshold_harris * dst.max()):
-            # Draw a circle on the imgHarris image for Harris corners
-            cv2.circle(imgHarris, (j, i), 3, (0, 0, 255), -1)  # Red color for Harris circles
+# Blur the grayscale image
+imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 0)
 
-# Plot the image with Harris corners
-plt.imshow(cv2.cvtColor(imgHarris, cv2.COLOR_BGR2RGB))
-plt.title('Harris Corners')
-plt.show()
+# Create a subplot with 2 rows and 3 columns
+plt.subplot(2, 3, 1), plt.imshow(cv2.cvtColor(imgOrig, cv2.COLOR_BGR2RGB))
+plt.title('Original'), plt.xticks([]), plt.yticks([])
+# Load the images
+img1 = cv2.imread(r'C:\Lab9\House.jpg')  # Update with the correct path if needed
+img2 = cv2.imread(r'C:\Lab9\ATU2.jpg')
 
-# Plot the result
-plt.imshow(cv2.cvtColor(imgHarris, cv2.COLOR_BGR2RGB))
-plt.title('Harris Corner Detection with Circles')
+plt.subplot(2, 3, 2), plt.imshow(imgGray, cmap='gray')
+plt.title('Grayscale'), plt.xticks([]), plt.yticks([])
+# Convert the images to grayscale
+gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
+plt.subplot(2, 3, 3), plt.imshow(imgBlur, cmap='gray')
+plt.title('Blurred'), plt.xticks([]), plt.yticks([])
 
-# Perform corner detection using Shi-Tomasi algorithm
-maxCorners = 100  # Experiment with different values
-qualityLevel = 0.01
-minDistance = 10
-corners = cv2.goodFeaturesToTrack(imgGray, maxCorners, qualityLevel, minDistance)
+# Apply Sobel detector to the grayscale image
+sobelx = cv2.Sobel(imgGray, cv2.CV_64F, 1, 0, ksize=5)
+sobely = cv2.Sobel(imgGray, cv2.CV_64F, 0, 1, ksize=5)
+imgSobel = np.sqrt(sobelx**2 + sobely**2)
 
-# Create a deep copy
-imgGFTT = copy.deepcopy(imgOrig)
+plt.subplot(2, 3, 4), plt.imshow(imgSobel, cmap='gray')
+plt.title('Sobel'), plt.xticks([]), plt.yticks([])
 
-# Convert to ints
-corners = np.int0(corners)
+# Apply Canny detector to the grayscale image
+imgCanny = cv2.Canny(imgGray, 100, 200)
 
-# Draw circles Shi-Tomasi
-for corner in corners:
-    x, y = corner.ravel()
-    cv2.circle(imgGFTT, (x, y), 3, (0, 255, 0), -1)  # Green color for GFTT circles
+plt.subplot(2, 3, 5), plt.imshow(imgCanny, cmap='gray')
+plt.title('Canny'), plt.xticks([]), plt.yticks([])
 
-# Plot the image with Shi-Tomasi corners
-plt.imshow(cv2.cvtColor(imgGFTT, cv2.COLOR_BGR2RGB))
-plt.title('Shi-Tomasi Corners')
+# ORB (Oriented FAST and Rotated BRIEF)
+# Initiate ORB detector
+orb = cv2.ORB_create()
+
+# Find the keypoints with ORB
+kp = orb.detect(imgGray, None)
+
+# Compute the descriptors with ORB
+kp, des = orb.compute(imgGray, kp)
+# Find the keypoints and descriptors with ORB
+kp1, des1 = orb.detectAndCompute(gray1, None)
+kp2, des2 = orb.detectAndCompute(gray2, None)
+
+# Draw only keypoints location, not size and orientation
+img_with_keypoints = cv2.drawKeypoints(imgGray, kp, None, color=(0, 255, 0), flags=0)
+# Brute-Force Matcher
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+matches_bf = bf.match(des1, des2)
+
+plt.subplot(2, 3, 6), plt.imshow(img_with_keypoints, cmap='gray')
+plt.title('ORB Keypoints'), plt.xticks([]), plt.yticks([])
+# Sort them in ascending order of distance
+matches_bf = sorted(matches_bf, key=lambda x: x.distance)
+
+# Adjust layout for better display
+plt.tight_layout()
+# Draw matches
+img_matches_bf = cv2.drawMatches(img1, kp1, img2, kp2, matches_bf[:10], None, flags=2)
+
+# Show the plot
+# Display the result
+plt.imshow(img_matches_bf)
+plt.title('BruteForceMatcher')
 plt.show()
